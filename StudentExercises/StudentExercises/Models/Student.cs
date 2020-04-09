8d2accf5f7c4ccf2d2714e2d2c3490e5a19f65cc
wggreen <wggreen.nss@gmail.com>
2020-04-09 16:06:18 -0500

    added view models to student exercises

---
+++ StudentExercises/StudentExercises/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string SlackHandle { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CohortId { get; set; }
+    }
+}
